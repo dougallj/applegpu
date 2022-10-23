798d2c633a7ed95360e96d6577ddce5e916fac06
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2022-10-23 13:09:58 -0400

    Identify sample mask argument to st_tile
    And add some docs. Examples:
    
      b2: 095000c4f1048000     st_tile          r20l_r20h_r21l_r21h, u8norm, 28, 0, quad, 0, 1, 0
     10a: 09200044f1fc8003     st_tile          r8l_r8h_r9l_r9h, u8norm, 20, 0, quad, 0, 255, 0
     1de: 097c4044f1048000     st_tile          r31l_r31h_r32l_r32h, u8norm, 20, 0, quad, 0, r0h, 0
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

--- applegpu.py
+++ applegpu.py
@@ -1831,6 +1831,29 @@ class MemoryBaseDesc(OperandDesc):
 		fields[self.name + 't'] = 1 if isinstance(r, UReg64) else 0
 		fields[self.name] = r.n << 1
 
+class SampleMaskDesc(OperandDesc):
+	def __init__(self, name):
+		super().__init__(name)
+		self.add_merged_field(self.name, [
+			(42, 6, self.name),
+			(56, 2, self.name + 'x'),
+		])
+		self.add_field(22, 2, self.name + 't')
+
+	def decode(self, fields):
+		flags = fields[self.name + 't']
+		value = fields[self.name]
+
+		if flags == 0b0:
+			return Immediate(value)
+		elif flags == 0b1:
+			return Reg16(value)
+		else:
+			assert(0)
+
+	def encode_string(self, fields, opstr):
+		assert(0)
+
 class MemoryRegDesc(OperandDesc):
 	def __init__(self, name, off=10, offx=40, offt=49):
 		super().__init__(name)
@@ -4052,6 +4075,23 @@ MASK_DESCRIPTIONS = {
 # uses sr60 implicitly?
 @register
 class LdstTileDesc(InstructionDesc):
+	documentation_html = '''
+	<p>
+	<code>ld_tile</code> and <code>st_tile</code> access shared memory in a
+	fragment shader with pixel-relative addressing. This emulates a
+	tilebuffer.
+
+	AGX fragment shaders always run at pixel rate (sample rate shading may
+	be lowered to pixel rate shading with a loop). That
+	means st_tile and ld_tile need to be able to load and store specific
+	samples. This is controlled by their sample mask source, which is a
+	bitmask of enabled samples. To store to a particular sample N, pass
+	(1 &lt;&lt; N). To store to a subset of samples { S1, ..., Sn },
+	pass Σ_i=1...n (1 &lt;&lt; N). To store to all samples, pass 0xFF as the
+	special "broadcast" value. Although MSAA 8x may not be used, this
+	value might be optimized specially.
+	</p>'''
+
 	def __init__(self):
 		super().__init__('TODO.ld/st_tile', size=(8, 10))
 
@@ -4071,8 +4111,7 @@ class LdstTileDesc(InstructionDesc):
 		self.add_operand(EnumDesc('mask', 36, 4, MASK_DESCRIPTIONS))
 
 		self.add_operand(ImmediateDesc('A', [(28, 4, 'A'), (40, 2, 'Ax')]))
-		self.add_operand(ImmediateDesc('B', [(42, 6, 'B'), (56, 2, 'Bx')]))
-		self.add_operand(ImmediateDesc('Br', 22, 2))
+		self.add_operand(SampleMaskDesc('S'))
 		self.add_operand(ImmediateDesc('C', [(16, 6, 'C'), (58, 2, 'Cx')]))
 
 	def map_to_alias(self, mnem, operands):
